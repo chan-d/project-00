f7d89edd8a72ea10ae967ebfba27e4af81804e51
chan-d <chan.danny516@gmail.com>
2015-11-25 20:58:43 -0800

    Game1 almost finished

--- Game1/main.js
+++ Game1/main.js
@@ -1,4 +1,55 @@
 $(document).ready(function() {
 
 	alert('the document is ready');
-});
+	$('.btn').click(function clearBoard(){
+	$('.box').empty();
+		posOne = 0;
+		posTwo = 0;
+	$('.start').append(playerOne);
+	$('.start').append(playerTwo);
+	});
+	var playerOne = 'x';
+	var playerTwo = 'o';
+	var posOne = 0;
+	var posTwo = 0;
+	$('.start').append(playerOne);
+	$('.start').append(playerTwo);
+
+	$(window).on('keypress', function handler (event) {
+		if(event.which === 97) {
+			if(posOne < 53) {
+				if ( $('.box').text() === playerOne&&playerTwo ) {
+  					$('.box').empty();
+  					$('.'+ posTwo).text(playerTwo);
+  					posOne++;
+  					$('.'+ posOne).append(playerOne);
+				} else if ($('.box').text() !== "" ){
+  					$('.box').empty();
+  					$('.'+ posTwo).text(playerTwo);
+  					posOne++;
+  					$('.'+ posOne).append(playerOne);
+				}
+			}
+		} else if (event.which === 108) {
+				if(posTwo < 53) {
+				if ( $('.box').text() === playerOne&&playerTwo ) {
+  					$('.box').empty();
+  					$('.'+ posOne).text(playerOne);
+  					posTwo++;
+  					$('.'+ posTwo).append(playerTwo);
+				} else if ($('.box').text() !== "" ){
+  					$('.box').empty();
+  					$('.'+ posOne).text(playerOne);
+  					posTwo++;
+  					$('.'+ posTwo).append(playerTwo);
+				}
+			}
+		}
+	}
+
+
+
+
+)});
+
+
